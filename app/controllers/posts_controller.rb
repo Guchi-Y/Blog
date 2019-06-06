8c3f05ff1dd8b417cec9641f7b31ccdd74210407
Guchi-Y <i25mas25@gmail.com>
2019-06-06 13:53:04 +0900

    Add redirect action
    ユーザー限定のページへ、URLを直接入力してのアクセスを禁止した

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,6 @@
 class PostsController < ApplicationController
+  before_action :move_to_index, except: [:index]
+  
   def index
     @posts = Post.all.order("created_at DESC")
   end 
@@ -10,5 +12,11 @@ class PostsController < ApplicationController
   def create
     Post.create(text: params[:post][:text], user_id: current_user.id)
   end
+
+  private
+  def move_to_index
+    redirect_to action: :index unless user_signed_in?
+  end  
 end
+
 
