65e16c8260949be50165ba2c332f9a5798fe961a
Guchi-Y <i25mas25@gmail.com>
2019-06-06 15:03:47 +0900

    Display the edit screen
    へんしゅうボタンを押した際に、編集画面へ移動し画面を表示する様にした

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   devise_for :users
   root 'posts#index'
-  get  'posts'        => 'posts#index'
-  get  'posts/new'    => 'posts#new'
-  post 'posts'        => 'posts#create'
+  get  'posts'          => 'posts#index'
+  get  'posts/new'      => 'posts#new'
+  post 'posts'          => 'posts#create'
+  get  'posts/:id/edit' => 'posts#edit'
 end
