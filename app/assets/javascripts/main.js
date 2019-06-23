19c866e7fdbfa5055d96266149171d6e8aaf5fee
Guchi-Y <i25mas25@gmail.com>
2019-06-23 10:58:38 +0900

    Implementation of original dialog
    投稿削除をする際にダイアログを出し、はい いいえ の選択をする様にした

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -1,18 +1,38 @@
 $(function() {
-  $(".edit_button").click(function() {
+  var html = `<div class="confirmation">
+  <h1>
+  とうこうが さくじょ されました
+  </h1>
+  <a class="button" href="/">とうこうがめんへ</a>
+  </div>`
+  
+  $(".delete_button").on("click", function() {
+    var postId   =  $(this).attr("data-post-id");
+    
     $("#dl").dialog({
       dialogClass:"wkDialogClass",
       modal:true,
       title:"かくにん",
       buttons:[
-       {text: "はい",
-        class:"wkBtnOk",
-        click: function() {$(this).dialog("close");}
-       },
-       {text: "いいえ",
+        {text: "はい",
+          class:"wkBtnOk",
+          click: function() {
+            
+            $.ajax({
+              url: `/posts/${postId}`,
+              type: 'DELETE'
+            })
+            
+            $(".contents").html(html);
+            $(this).dialog("close");
+          }
+        },
+        
+        {text: "いいえ",
         class:"wkBtnNg",
         click: function() {$(this).dialog("close");}
-       }]
+      }]
     });
+  return false;
   });
 });
